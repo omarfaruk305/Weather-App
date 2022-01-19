65ebe460d8dc95ec93555dfb317e6416f36d9a2c
Ömer Faruk Uygur <omarfarukuygur@gmail.com>
2022-01-19 21:30:37 +0100

    created exe and created main

--- Display.py
+++ Display.py
@@ -1,15 +1,14 @@
+
 from PyQt5.QtWidgets import (QWidget, QSlider, QLineEdit, QLabel, QPushButton, QScrollArea, QApplication,
                              QHBoxLayout, QVBoxLayout, QFileDialog, QMainWindow)
-import mn
+
 from PyQt5 import QtCore
 from PyQt5.QtCore import Qt, QSize
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtGui import QPixmap
 import sys
 import api
-from db import get_tables
-from db import get_city_information
-
+import db
 
 class UI(QtWidgets.QMainWindow):
 
@@ -34,7 +33,7 @@ class UI(QtWidgets.QMainWindow):
         try:
             # capitalize olmuyor, herbir kelime buyukle baslamali
             cityname = self.lineEdit.text().title()
-            city_gui = get_city_information(f"{cityname}")
+            city_gui = db.get_city_information(f"{cityname}")
             self.cityname_gui.setText(city_gui[0][0])
             self.provincename_gui.setText(city_gui[0][1])
             self.population_gui.setText(str(city_gui[0][2]))
@@ -42,7 +41,8 @@ class UI(QtWidgets.QMainWindow):
             parameters = api.info.get_weather(f'{cityname}')
             self.pixmap = QPixmap(f"image\\{parameters[4]}.png")
             self.label.setPixmap(self.pixmap)
-            self.code.setText(f"on {parameters[5]}, in {parameters[0]},  {parameters[1]}")
+            self.code.setText(
+                f"on {parameters[5]}, in {parameters[0]},  {parameters[1]}")
             self.degree.setText(f"{parameters[2]}")
             self.description.setText(f"{parameters[3].title()}")
         except Exception as a:
@@ -65,7 +65,8 @@ class UI(QtWidgets.QMainWindow):
         for i in country_name:
             self.tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(i[0]))
             self.tableWidget.setItem(row, 1, QtWidgets.QTableWidgetItem(i[1]))
-            self.tableWidget.setItem(row, 2, QtWidgets.QTableWidgetItem(str(i[2])))
+            self.tableWidget.setItem(
+                row, 2, QtWidgets.QTableWidgetItem(str(i[2])))
             row += 1
 
     def TABLO(self, konum, y):
@@ -73,10 +74,10 @@ class UI(QtWidgets.QMainWindow):
         for location in konum.indexes():
             row = location.row()
             column = location.column()
-            kontrol = UIWindow.tableWidget.item(row, column)
+            kontrol = self.tableWidget.item(row, column)
             # print(row,column)
             if kontrol and column == 0:
-                city_name = UIWindow.tableWidget.item(row, column).text()
+                city_name = self.tableWidget.item(row, column).text()
                 self.lineEdit.setText(city_name)
                 self.clicker()
 
@@ -84,25 +85,25 @@ class UI(QtWidgets.QMainWindow):
         self.pixmap2 = QPixmap("image\\USAflag.png")
         self.label_7.setPixmap(self.pixmap2)
 
-        display_table = get_tables("America")
+        display_table = db.get_tables("America")
         self.set_country_table(display_table)
 
     def country_selector_2(self):
-        display_table = get_tables("Netherlands")
+        display_table = db.get_tables("Netherlands")
         self.pixmap2 = QPixmap("image\\NLflag.jpg")
         self.label_7.setPixmap(self.pixmap2)
         self.set_country_table(display_table)
 
     def country_selector_3(self):
-        display_table = get_tables("Turkey")
+        display_table = db.get_tables("Turkey")
         self.pixmap2 = QPixmap("image\\TRflag.jpg")
         self.label_7.setPixmap(self.pixmap2)
         self.set_country_table(display_table)
 
     def revert(self):
-        self.tableWidget.clearContents()#cleans table,
+        self.tableWidget.clearContents()  # cleans table,
         self.tableWidget.setRowCount(0)
-        self.tableWidget.setColumnCount(3)#3 column remain
+        self.tableWidget.setColumnCount(3)  # 3 column remain
         self.pixmap3 = QPixmap("image\sky.jpeg")
         self.label_7.setPixmap(self.pixmap3)
         self.degree.setText("-")
@@ -118,6 +119,3 @@ class UI(QtWidgets.QMainWindow):
         self.lineEdit.setText("City Name")
 
 
-app = QApplication(sys.argv)
-UIWindow = UI()
-app.exec_()
